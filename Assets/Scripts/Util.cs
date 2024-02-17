ffa598bb86fb49c7f64a34d4a4cf6225cbbe044c
Mads Wrang <32076614+AZ0N@users.noreply.github.com>
2024-02-17 18:35:31 +0100

    extract IsValidCoordinate method

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -1,14 +1,20 @@
+using System;
+
 public class Util
 {
 	static char[] files = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
 
 	public static string CoordinateToString(int rank, int file)
 	{
-		if (rank < 0 || 8 <= rank || file < 0 || 8 <= file)
+		if (!IsValidCoordinate(rank, file))
 		{
-			// TODO Handle invalid coordinate
-			return "??";
+			throw new ArgumentException($"Invalid rank and file: rank {rank}, file {file}");
 		}
 		return $"{files[file]}{rank + 1}";
+	}
+
+	public static bool IsValidCoordinate(int rank, int file)
+	{
+		return rank < 0 || 8 <= rank || file < 0 || 8 <= file;
 	}
 }
