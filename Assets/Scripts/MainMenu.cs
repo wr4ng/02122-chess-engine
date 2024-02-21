b268332904491b8af590a23fde64f65953d1d5cc
Mads Wrang <32076614+AZ0N@users.noreply.github.com>
2024-02-21 13:42:27 +0100

    update main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,34 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
+
+enum SceneIndex
+{
+	MainMenu = 0,
+	Main = 1
+}
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField, Tooltip("Input field to pass on FEN or PGN notation")] private TMP_Text inputFieldCo;
+	[SerializeField, Tooltip("Input field to pass on FEN or PGN notation")]
+	private TMP_Text inputFieldCo;
 
-    
+	public void PlayGame()
+	{
+		SceneManager.LoadScene((int)SceneIndex.Main);
+	}
 
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+	public void QuitGame()
+	{
+#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+#endif
+		Application.Quit();
+	}
 
-    public void QuitGame()
-    {
-        Debug.Log("Quit");
-        Application.Quit();
-    }
+	// Takes the imput from the text field and sets the position
+	public void playWithInput()
+	{
+		string input = inputFieldCo.text;
+		Debug.Log(input);
 
-    // Takes the imput from the text field and sets the position 
-    public void playWithInput()
-    {
-        string input = inputFieldCo.text;
-        Debug.Log(input);
-
-        // TODO : Add the logic to set the position from the input and check if it is valid FEN or PGN notation
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+		// TODO : Add the logic to set the position from the input and check if it is valid FEN or PGN notation
+		SceneManager.LoadScene((int)SceneIndex.Main);
+	}
 }
