d7eb1794d064bc33e25da73f2830fc26e3c1e3d4
DeMoManique <frederik.hvarregaard.andersen@gmail.com>
2024-04-10 16:16:23 +0200

    added piece positions values

--- Assets/Scripts/Bot/Evaluation.cs
+++ Assets/Scripts/Bot/Evaluation.cs
@@ -24,34 +24,36 @@ namespace Bot
                     Piece piece = board.GetPiece(file, rank);
                     if (piece != null)
                     {
-                        score += PieceWieght(piece);
+                        score += PieceWeight(piece);
+                        score += PiecePosition(piece, file+ 8 * rank);
                     }
                 }
             }
             return score;
         }
-        public static float PieceWieght(Piece piece)
+
+        public static float PieceWeight(Piece piece)
         {
             float weight;
             switch (piece.GetPieceType())
             {
                 case PieceType.Pawn:
-                    weight = (piece.GetColor() == Color.White) ? 1 : -1;
+                    weight = (piece.GetColor() == Color.White) ? 10 : -10;
                     break;
                 case PieceType.Rook:
-                    weight = (piece.GetColor() == Color.White) ? 5 : -5;
+                    weight = (piece.GetColor() == Color.White) ? 50 : -50;
                     break;
                 case PieceType.Bishop:
-                    weight = (piece.GetColor() == Color.White) ? 3 : -3;
+                    weight = (piece.GetColor() == Color.White) ? 30 : -30;
                     break;
                 case PieceType.Knight:
-                    weight = (piece.GetColor() == Color.White) ? 3 : -3;
+                    weight = (piece.GetColor() == Color.White) ? 30 : -30;
                     break;
                 case PieceType.Queen:
-                    weight = (piece.GetColor() == Color.White) ? 9 : -9;
+                    weight = (piece.GetColor() == Color.White) ? 90 : -90;
                     break;
                 case PieceType.King:
-                    weight = (piece.GetColor() == Color.White) ? 100 : -100;
+                    weight = (piece.GetColor() == Color.White) ? 1000 : -1000;
                     break;
                 default:
                     weight = 0;
@@ -60,9 +62,34 @@ namespace Bot
             return weight;
         }
 
-        public float PiecePosition(Board board)
+        public static float PiecePosition(Piece piece, int index)
         {
-            throw new NotImplementedException();
+            float weight;
+            switch (piece.GetPieceType())
+            {
+                case PieceType.Pawn:
+                    weight = (piece.GetColor() == Color.White) ? PositionWeights.pawnWeight[63 - index] : -PositionWeights.pawnWeight[index];
+                    break;
+                case PieceType.Rook:
+                    weight = (piece.GetColor() == Color.White) ? PositionWeights.rookWeight[63 - index] : -PositionWeights.rookWeight[index];
+                    break;
+                case PieceType.Bishop:
+                    weight = (piece.GetColor() == Color.White) ? PositionWeights.bishopWeight[63 - index] : -PositionWeights.bishopWeight[index];
+                    break;
+                case PieceType.Knight:
+                    weight = (piece.GetColor() == Color.White) ? PositionWeights.knightWeight[63 - index] : -PositionWeights.knightWeight[index];
+                    break;
+                case PieceType.Queen:
+                    weight = (piece.GetColor() == Color.White) ? PositionWeights.queenWeight[63 - index] : -PositionWeights.queenWeight[index];
+                    break;
+                case PieceType.King:
+                    weight = (piece.GetColor() == Color.White) ? PositionWeights.kingWeight[63 - index] : -PositionWeights.kingWeight[index];
+                    break;
+                default:
+                    weight = 0;
+                    break;
+            }
+            return weight;
         }
 
         //Other evaluation functions
